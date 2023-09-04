c9ac508bc3125f4fe910525223d3f69024ab4fe7
Daisuke <nakada0907@outlook.com>
2023-09-04 14:06:03 +0900

    revised path of program

--- simpost.sh
+++ simpost.sh
@@ -5,7 +5,7 @@ set -e
 export OPENBLAS_NUM_THREADS=1
 export FOR_COARRAY_NUM_IMAGES=3
 
-$HOME/software/1d_sediment_river_model/build/ifx_F08404EA8A622646/app/sediment_river_model_1d
+sediment_river_model_1d
 
 readonly target_dist_name=`basename ${PWD}`
 
